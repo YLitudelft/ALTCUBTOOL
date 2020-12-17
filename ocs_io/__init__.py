2f7eb5bfb68e049946156ab2ad67fb7d02c6b052
YLitudelft <44788969+YLitudelft@users.noreply.github.com>
2020-12-17 09:54:07 +0100

    Add files via upload

---
+++ ocs_io/__init__.py
@@ -0,0 +1,25 @@
+"""
+=================================
+I/O Utilities (:mod:`osiris.ocs_io`)
+=================================
+
+.. currentmodule:: osiris.ocs_io
+
+This module contains I/O Utilities
+
+State Files
+-----------
+.. toctree::
+   :maxdepth: 2
+
+.. automodule:: osiris.ocs_io.netcdf
+   :members:
+
+.. automodule:: osiris.ocs_io.state
+   :members:
+
+.. automodule:: osiris.ocs_io.cfg
+   :members:
+"""
+
+from .cfg import ConfigFile
